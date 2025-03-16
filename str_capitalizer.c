510916d0d8f115b2d8cc10087f5860de94a9052e
sandy33 <sarahalfar2000@gmail.com>
2025-03-16 22:02:43 +0300

    Create str_capitalizer.c

---
+++ str_capitalizer.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+void    str_capitalizer(char *str)
+{
+    int i = 0;
+    int new = 1;
+    while(str[i])
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+        {
+             str[i] += 32;
+        }
+        if(str[i] >= 'a' && str[i] <= 'z' && new)
+        {
+            str[i] -= 32;
+            new = 0;
+        }
+        if(str[i] == ' ')
+        {
+            new = 1;
+        }
+        if (!(str[i] >= 'a' && str[i] <= 'z') && !(str[i] >= 'A' && str[i] <= 'Z') && !(str[i] >= '0' && str[i] <= '9') && str[i] != ' ')
+        {
+            new = 0;
+        }
+
+        write(1, &str[i], 1);
+        i++;
+    }
+}
+
+int main()
+{
+    char str[] = "__SecONd teST A LITtle BiT   Moar comPLEX" "   But... This iS not THAT COMPLEX" "     Okay, this is the last 1239809147801 but not    the least    t" ;
+    str_capitalizer(str);
+}
