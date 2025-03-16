584e522b255acbc85040767e97c84f076429e168
sandy33 <sarahalfar2000@gmail.com>
2025-03-16 21:37:59 +0300

    Create search_and_replace.c

---
+++ search_and_replace.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+void    ft_putstr(char *str)
+{
+    int i = 0;
+    while(str[i])
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
+void    search_and_replace(char *str, char a, char b)
+{
+    int i = 0;
+    while(str[i])
+    {
+        if(str[i] == a)
+        {
+            str[i] = b;
+        }
+        i++;
+    }
+    ft_putstr(str);
+}
+
+int main()
+{
+    char str[] = "sarah";
+    char *a = "a";
+    char *o = "o";
+    if(o[1] == '\0' && a[1] == '\0')
+    {
+        search_and_replace(str, 'a', 'o');
+    }
+    else
+        write(1, "\n", 1);
+}
